fa80a9989e9adf99ef250941a53bccae5ef19665
Daryn <daryn.holmes@insightsoftware.com>
2017-09-18 19:22:17 +0100

    solution

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyparser = require('body-parser')
-const { graphqlExpress } = require('apollo-server-express')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
 
 
@@ -26,6 +26,8 @@ const graphQLSchema =  makeExecutableSchema({
 });
 
 app.get('/status', (req, res) => res.send('Express status: OK'))
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.use('/graphql', bodyparser.json(), graphqlExpress({ schema: graphQLSchema }))
 
 console.log(`Express running on ${PORT}`)
