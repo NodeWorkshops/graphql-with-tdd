e1b7cac36810a0e4cbf82e476a8dcdbf920e2c6f
DarynHolmes <DarynHolmes@gmail.com>
2017-09-23 21:53:36 +0100

    100-test-configuration-solution

--- src/__tests__/appTest.js
+++ src/__tests__/appTest.js
@@ -1,15 +1,7 @@
 // ./src/appTest.js
 
-const request = require('supertest')
-const app = require('../app')
-
-describe('Graphql status', () => {
-    test('It should respond with a success message', async () => {
-        const response = await request(app)
-            .post('/graphql')
-            .send({'query': 'query  { status }'})
-        expect(response.statusCode).toBe(200)
-        expect(response.body.data.status).toBe("GrpahQL status: OK")
+describe('Example test', () => {
+    test('It should pass', async () => {
+        expect(1).toBe(1)
     });
  });
- 
