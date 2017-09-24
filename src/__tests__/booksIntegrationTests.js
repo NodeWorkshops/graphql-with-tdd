73903fa910bc90593d70c60590c84056beac922c
DarynHolmes <DarynHolmes@gmail.com>
2017-09-24 15:52:24 +0100

    solution

---
+++ src/__tests__/booksIntegrationTests.js
@@ -0,0 +1,15 @@
+// ./src/booksIntegrationTests.js
+
+const request = require('supertest')
+const app = require('../app')
+
+describe('Fetching books', () => {
+    test('It should respond with a list of all the books', async () => {
+        const response = await request(app)
+            .post('/graphql')
+            .send({'query': 'query  { books }'})
+        expect(response.statusCode).toBe(200)
+        expect(response.body.data.length).toBe(10)
+    });
+ });
+ 
