6053c8d62d354fdbc64f23f6cdd630b77676a19f
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-08-21 14:30:43 -0700

    test: add basic snapshot test for graphql route

--- src/__tests__/appTest.js
+++ src/__tests__/appTest.js
@@ -1,5 +1,13 @@
-describe("Example test", () => {
-    test("It should pass", async () => {
-        expect(1).toBe(1);
-    })
-})
+const request = require("supertest");
+const app = require("../app");
+
+describe("Graphql status", () => {
+  test("It should respond with a success message", async () => {
+    const response = await request(app)
+      .post("/graphql")
+        .send({ query: "query { status { message } }" });
+      
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toMatchSnapshot();
+  });
+});
