ebb0b72f69e71f7b6568b20218ce190f7210f9db
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-08-21 13:58:03 -0700

    refactor: split graphql code into app and server files

---
+++ src/__tests__/appTest.js
@@ -0,0 +1,5 @@
+describe("Example test", () => {
+    test("It should pass", async () => {
+        expect(1).toBe(1);
+    })
+})
--- src/server.js
+++ src/server.js
@@ -4,85 +4,8 @@ const express = require("express");
 const { graphqlExpress, ApolloServer, gql } = require("apollo-server-express");
 const moment = require("moment");
 const os = require("os");
+const app = require("./app");
 
 const PORT = 3000;
 
-const app = express();
-
-const Query = gql`
-  type Query {
-    status: Status
-    cpuDetails(cpuNumber: Int): CPU
-    cpuList: [CPU]
-  }
-`;
-
-const Mutation = gql`
-  type Mutation {
-    _empty: String
-  }
-`;
-
-const Status = gql`
-  type Status {
-    code: Int
-    message: String
-    lastUpdated: String
-  }
-`;
-
-const CPU = gql`
-  type CPU {
-    model: String
-    speed: String
-    times: Times
-  }
-`;
-const Times = gql`
-  type Times {
-    user: Int
-    nice: Int
-    sys: Int
-    idle: Int
-    irq: Int
-  }
-`;
-
-
-let resolvers = {
-  Query: {
-    status: () => ({ code: 200, msg: "ok" }),
-    cpuDetails: (obj, args) => os.cpus()[args.cpuNumber],
-    cpuList: () => os.cpus()
-  },
-  Status: {
-    message: (obj) => obj.msg,
-    lastUpdated: () => moment().format(),
-  },
-  /*CPU: {
-    model: (cpu) => cpu.model,
-    speed: (cpu) => cpu.speed,
-    times: (cpu) => cpu.times,
-  },*/  // Each CPU field is a trivial resolver, so we don't need to explicitly write them
-  /*Times: {
-    user: (times) => times.user,
-    nice: (times) => times.nice,
-    sys: (times) => times.sys,
-    idle: (times) => times.idle,
-    irq: (times) => times.irq,
-  },*/
-};
-
-const typeDefs = [Query, Mutation, Status, CPU, Times];
-
-const graphQLSchema = new ApolloServer({
-  typeDefs,
-  resolvers,
-}); // TODO
-
-app.get("/status", (req, res) => res.send("Express status: OK"));
-// app.use("graphql", graphqlExpress({ schema: graphQLSchema }));
-graphQLSchema.applyMiddleware({ app });
-
-console.log(`Express running on ${PORT}`);
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Express running on ${PORT}`));
